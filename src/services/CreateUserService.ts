007169dd876a0cd9b678f9d4aed41cdb39f14be1
Murilo Andrade <muriloap.info@gmail.com>
2021-06-28 07:43:55 -0300

    Finaliza a aula 2 - Funcionalidades para inserção de novo usuário

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UserRepositories";
+
+type UserRequestType = {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  
+  async execute({name, email, admin} : UserRequestType) {
+    
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    if(!email) {
+      throw new Error("Incorrect email");
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({email});
+
+    if(userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = usersRepository.create({name, email, admin});
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export {CreateUserService};
